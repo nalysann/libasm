13ee885f90a05fef6e369a2d6b65c322061a04f3
x33nu <urbilya@gmail.com>
2021-05-23 18:50:37 +0300

    ADd basic list functions

--- src/ft_list_push_front_bonus.s
+++ src/ft_list_push_front_bonus.s
@@ -1,1 +1,23 @@
 global _ft_list_push_front
+
+extern _malloc
+
+section .text
+
+_ft_list_push_front:
+    push    rbp             ; save rbp
+    mov     rbp, rsi        ; save data
+    push    rbx             ; save rbx
+    mov     rbx, rdi        ; save head
+    mov     rdi, 16         ; prepare arg for malloc
+    call    _malloc         ; node = malloc(sizeof(t_list))
+    test    rax, rax        ; if malloc failed
+    je      .clean          ; clean up and return
+    mov     [rax], rbp      ; node->data = data
+    mov     rdi, [rbx]      ; *head
+    mov     [rax+8], rdi    ; node->next = *head
+    mov     [rbx], rax      ; *head = node
+.clean:
+    pop     rbx             ; load rbx
+    pop     rbp             ; load rbp
+    ret                     ; return
--- src/ft_list_size_bonus.s
+++ src/ft_list_size_bonus.s
@@ -1,1 +1,15 @@
 global _ft_list_size
+
+section .text
+
+_ft_list_size:
+    xor     rax, rax        ; size = 0
+    test    rdi, rdi        ; if head == NULL
+    je      .return         ; empty list
+.loop:
+    mov     rdi, [rdi+8]    ; head = head->next
+    add     rax, 1          ; ++size
+    test    rdi, rdi        ; if head != NULL
+    jne     .loop           ; keep going
+.return:
+    ret                     ; return size
