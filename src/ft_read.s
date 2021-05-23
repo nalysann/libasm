9e72b30ad20349298cd9635fae5e6eef605272d3
x33nu <urbilya@gmail.com>
2021-05-23 18:48:42 +0300

    Add ft_read.s and ft_write.s

--- src/ft_read.s
+++ src/ft_read.s
@@ -1,1 +1,20 @@
 global _ft_read
+
+extern ___error
+
+section .text
+
+_ft_read:
+    mov     rax, 0x2000003  ; set syscall id
+    syscall                 ; call read
+    jc      .error          ; handle error
+    ret
+.error:
+    push    rbx             ; save rbx
+    mov     rbx, rax        ; save error
+    call    ___error        ; get errno address
+    mov     [rax], rbx      ; set errno
+    xor     rax, rax        ; set return value
+    not     rax             ; to -1
+    pop     rbx             ; load rbx
+    ret
--- src/ft_write.s
+++ src/ft_write.s
@@ -1,1 +1,20 @@
 global _ft_write
+
+extern ___error
+
+section .text
+
+_ft_write:
+    mov     rax, 0x2000004  ; set syscall id
+    syscall                 ; call write
+    jc      .error          ; handle error
+    ret
+.error:
+    push    rbx             ; save rbx
+    mov     rbx, rax        ; save error
+    call    ___error        ; get errno address
+    mov     [rax], rbx      ; set errno
+    xor     rax, rax        ; set return value
+    not     rax             ; to -1
+    pop     rbx             ; load rbx
+    ret
